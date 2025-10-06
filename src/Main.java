ae5a8ba20e207febc9fdf1493d633fcd0c36e7b8
syahfrinorezky <syahfrinorezky@gmail.com>
2025-10-06 18:09:05 +0800

    feat: add menu for input data & list data

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,62 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ArrayList<CD> daftarCD =  new ArrayList<>();
+        ArrayList<CD> daftarCD = new ArrayList<>();
         ArrayList<Customer> daftarCustomer = new ArrayList<>();
         ArrayList<Peminjaman> daftarPeminjaman = new ArrayList<>();
+        int pilih;
 
+        do {
+            System.out.println("========= MENU PEMINJAMAN CD ==========");
+            System.out.println("1. Input Satu Peminjaman");
+            System.out.println("2. Input Banyak Peminjaman");
+            System.out.println("3. Tampilkan Semua Data");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih Menu : ");
+            pilih = input.nextInt();
+            input.nextLine();
+
+            switch (pilih) {
+                case 1:
+                    tambahPeminjaman(input, daftarCD, daftarCustomer, daftarPeminjaman);
+                    break;
+
+                case 2:
+                    System.out.print("Berapa jumlah data yang ingin diinput: ");
+                    int jumlah = input.nextInt();
+                    input.nextLine();
+                    for (int i = 0; i < jumlah; i++) {
+                        System.out.println("\nInput data ke-" + (i + 1));
+                        tambahPeminjaman(input, daftarCD, daftarCustomer, daftarPeminjaman);
+                    }
+                    break;
+
+                case 3:
+                    if (daftarPeminjaman.isEmpty()) {
+                        System.out.println("Belum ada data peminjaman.\n");
+                    } else {
+                        System.out.println("========== DAFTAR PEMINJAMAN ==========");
+                        System.out.println();
+                        for (int i = 0; i < daftarPeminjaman.size(); i++) {
+                            System.out.println("Peminjaman ke-" + (i + 1));
+                            daftarPeminjaman.get(i).info();
+                            System.out.println();
+                        }
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Program selesai.");
+                    break;
+
+                default:
+                    System.out.println("Pilihan tidak valid.\n");
+                    break;
+            }
+        } while (pilih != 4);
+    }
+
+    static void tambahPeminjaman(Scanner input, ArrayList<CD> daftarCD, ArrayList<Customer> daftarCustomer, ArrayList<Peminjaman> daftarPeminjaman) {
         System.out.println("========= INPUT PEMINJAMAN ==========");
         System.out.print("Masukkan Nama CD       : ");
         String namaCd = input.nextLine();
@@ -32,7 +84,7 @@ public class Main {
         daftarCustomer.add(customer);
         daftarPeminjaman.add(peminjaman);
 
-        daftarPeminjaman.get(0).info();
-
+        System.out.println();
+        System.out.println("Data peminjaman berhasil ditambahkan.\n");
     }
 }
