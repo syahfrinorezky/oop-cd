d6cb56c2f3ca9c95421078a92471501cfbfbee5e
syahfrinorezky <syahfrinorezky@gmail.com>
2025-10-06 19:32:27 +0800

    chore: move tambahPeminjaman method to upline

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,35 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
+    static void tambahPeminjaman(Scanner input, ArrayList<CD> daftarCD, ArrayList<Customer> daftarCustomer, ArrayList<Peminjaman> daftarPeminjaman) {
+        System.out.println("========= INPUT PEMINJAMAN ==========");
+        System.out.print("Masukkan Nama CD       : ");
+        String namaCd = input.nextLine();
+        System.out.print("Masukkan Tahun CD      : ");
+        String tahunCd = input.nextLine();
+        System.out.print("Masukkan Jumlah Pinjam : ");
+        int jumlahPinjam = input.nextInt();
+        input.nextLine();
+
+        System.out.print("Nama Peminjam          : ");
+        String namaPeminjam = input.nextLine();
+        System.out.print("No KTP Peminjam        : ");
+        String noKtp = input.nextLine();
+        System.out.print("Alamat Peminjam        : ");
+        String alamatPeminjam = input.nextLine();
+
+        CD cd = new CD(namaCd, tahunCd, jumlahPinjam);
+        Customer customer = new Customer(namaPeminjam, noKtp, alamatPeminjam);
+        Peminjaman peminjaman = new Peminjaman(cd, customer);
+
+        daftarCD.add(cd);
+        daftarCustomer.add(customer);
+        daftarPeminjaman.add(peminjaman);
+
+        System.out.println();
+        System.out.println("Data peminjaman berhasil ditambahkan.\n");
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         ArrayList<CD> daftarCD = new ArrayList<>();
@@ -59,32 +88,4 @@ public class Main {
         } while (pilih != 4);
     }
 
-    static void tambahPeminjaman(Scanner input, ArrayList<CD> daftarCD, ArrayList<Customer> daftarCustomer, ArrayList<Peminjaman> daftarPeminjaman) {
-        System.out.println("========= INPUT PEMINJAMAN ==========");
-        System.out.print("Masukkan Nama CD       : ");
-        String namaCd = input.nextLine();
-        System.out.print("Masukkan Tahun CD      : ");
-        String tahunCd = input.nextLine();
-        System.out.print("Masukkan Jumlah Pinjam : ");
-        int jumlahPinjam = input.nextInt();
-        input.nextLine();
-
-        System.out.print("Nama Peminjam          : ");
-        String namaPeminjam = input.nextLine();
-        System.out.print("No KTP Peminjam        : ");
-        String noKtp = input.nextLine();
-        System.out.print("Alamat Peminjam        : ");
-        String alamatPeminjam = input.nextLine();
-
-        CD cd = new CD(namaCd, tahunCd, jumlahPinjam);
-        Customer customer = new Customer(namaPeminjam, noKtp, alamatPeminjam);
-        Peminjaman peminjaman = new Peminjaman(cd, customer);
-
-        daftarCD.add(cd);
-        daftarCustomer.add(customer);
-        daftarPeminjaman.add(peminjaman);
-
-        System.out.println();
-        System.out.println("Data peminjaman berhasil ditambahkan.\n");
-    }
 }
